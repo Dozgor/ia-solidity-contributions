1378f12594cebe70230a36aa489064f7ffcbaa31
Dozgor <119602202+Dozgor@users.noreply.github.com>
2025-06-13 09:24:56 +0200

    Update SimpleVault.sol

--- SimpleVault/contracts/SimpleVault.sol
+++ SimpleVault/contracts/SimpleVault.sol
@@ -3,12 +3,36 @@ pragma solidity ^0.8.20;
 
 contract SimpleVault {
     mapping(address => uint256) public balances;
+    address public admin;
+    bool public paused = false;
+    receive() external payable {}
+    constructor(){
+        admin = msg.sender;
+    }
 
-    function deposit() external payable {
+    modifier whenNotPaused() {
+    require(!paused, "Contrat en pause");
+    _;
+    }
+
+    modifier onlyAdmin(){
+        require(msg.sender == admin,"Caller is not the admin!");
+        _;
+    }
+
+    function pause() public onlyAdmin {
+    paused = true;
+    }
+
+    function unpause() public onlyAdmin {
+    paused = false;
+    }
+
+    function deposit() external payable whenNotPaused{
         balances[msg.sender] += msg.value;
     }
 
-    function withdraw() external {
+    function withdraw() external whenNotPaused onlyAdmin{
         uint256 amount = balances[msg.sender];
         require(amount > 0, "Nothing to withdraw");
 
