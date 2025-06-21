e4784c12477e7b101fbb5ace79419ea9ded90df8
Dozgor <119602202+Dozgor@users.noreply.github.com>
2025-06-21 21:26:45 +0200

    Update QuizDevinette.sol

--- QuizDevinette/QuizDevinette.sol
+++ QuizDevinette/QuizDevinette.sol
@@ -5,5 +5,25 @@ contract QuizDevinette {
     address public owner;
     string public question;
     bytes32 private reponseHash;
+    
+    constructor() {
+        owner = msg.sender;
+    }
 
+    function poserDevinette(string memory _question, string memory _reponseClaire) public {
+        require(msg.sender == owner, "Seul le proprietaire peut poser la devinette");
+        question = _question;
+        reponseHash = keccak256(abi.encodePacked(_reponseClaire));
+    }
+
+    event ReponseTrouvee(address gagnant);
+
+    function deviner(string memory proposition) public {
+        require(bytes(question).length != 0, "Aucune devinette n'est active");
+    
+        if (keccak256(abi.encodePacked(proposition)) == reponseHash) {
+        emit ReponseTrouvee(msg.sender);
+        }
+    }
+}
     
